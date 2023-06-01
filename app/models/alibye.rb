f10c283c0bedafe88d323526aeb260c590c24a89
Nestor Martinez <nesturron@gmail.com>
2023-06-01 14:33:47 +0200

    Fixed the models not to have the errors with the seed

--- app/models/alibye.rb
+++ app/models/alibye.rb
@@ -2,6 +2,7 @@ class Alibye < ApplicationRecord
   CATEGORIES = %w[Work Family Relationships Murder Fraud Other Insurance].sort
   belongs_to :user
   has_many :users, through: :bookings
+  has_many :bookings, dependent: :destroy
   validates :title, :description, :price_per_hour, presence: true
   validates :description, length: { minimum: 6 }
   validates :category, inclusion: {
